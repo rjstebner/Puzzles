76abf80dfdc9fec63d930d6da844896de3266176
DiabGarren <garrendiab@gmail.com>
2024-02-23 06:41:53 +0200

    Added comments and docs

--- Models/CartModel.cs
+++ Models/CartModel.cs
@@ -1,22 +1,64 @@
 namespace Puzzles;
 
-
+/// <summary>
+/// Class <c>Cart</c> is the main Cart object for storing the current cart. The class has two public properties: CartPuzzles, Cart Total and one public method: GetFormattedTotal().
+/// </summary>
 public class Cart
 {
+    /// <summary>
+    /// <value>Property <c>CartPuzzles</c> is a <c>CartPuzzle</c> list that contains all the puzzles in the cart.</value> 
+    /// </summary>
     public List<CartPuzzle> CartPuzzles { get; set; } = [];
+
+    /// <summary>
+    /// <value>Property <c>CartTotal</c> is a decimal to store the running total of the cart.</value>
+    /// </summary>
     public decimal CartTotal { get; set; }
+
+    /// <returns>The cart total as a string with the currency indicator in the front.</returns>
     public string? GetFormattedTotal() => CartTotal.ToString("C");
 }
 
+/// <summary>
+/// Class <c>CartPuzzle</c> is used as an object class in Class <c>Cart</c> as a list to store all the puzzles added to the cart. The class has five public properties: Id, Name, ImageUrl, BasePrice, and Quantity and one public method: GetFormattedBasePrice.
+/// </summary>
 public class CartPuzzle
 {
+    /// <summary>
+    /// <value>Proprty<c>Id</c> is an integer representing the puzzle's Id.</value>
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// <value>Proprty<c>Name</c> is a string representing the puzzle's name.</value>
+    /// </summary>
     public string? Name { get; set; }
+
+    /// <summary>
+    /// <value>Proprty<c>ImageUrl</c> is a string representing the url to the puzzle's image.</value>
+    /// </summary>
     public string? ImageUrl { get; set; }
+
+    /// <summary>
+    /// <value>Proprty<c>Id</c> is a decimal representing the puzzle's base price.</value>
+    /// </summary>
     public decimal BasePrice { get; set; }
+
+    /// <summary>
+    /// <value>Proprty<c>Id</c> is an integer representing the amount of each puzzle in the cart.</value>
+    /// </summary>
     public int Quantity { get; set; }
+
+    /// <returns>The base price as a string with the currency indicator in the front.</returns>
     public string? GetFormattedBasePrice() => BasePrice.ToString("C");
 
+    /// <summary>
+    /// The Constructor of class <c>CartPuzzle</c> that sets the values of each property
+    /// </summary>
+    /// <param name="Id">The puzzle's Id.</param>
+    /// <param name="Name">The puzzle's name.</param>
+    /// <param name="ImageUrl">The url to the puzzle's image.</param>
+    /// <param name="BasePrice">The puzzle's base price</param>
     public CartPuzzle(int Id, string Name, string ImageUrl, decimal BasePrice)
     {
         this.Id = Id;
--- Services/CartState.cs
+++ Services/CartState.cs
@@ -1,25 +1,51 @@
 namespace Puzzles.Services;
 
+/// <summary>
+/// Class <c>CartState</c> handles the current state of the <c>Cart</c> and any methods for manipulating the <c>CartPuzzles</c> list
+/// </summary>
 public class CartState
 {
+    /// <summary>
+    /// <value>Property <c>Cart</c> is a <c>Cart</c> object that contains the current cart.</value> 
+    /// </summary>
     public Cart Cart { get; private set; } = new Cart();
 
-
+    /// <summary>
+    /// Adds the given puzzle to the cart.
+    /// </summary>
+    /// <param name="puzzle">A PuzzleList object.</param>
     public void AddToCart(PuzzleList puzzle)
     {
+        // Create a new CartPuzzle object
         CartPuzzle newPuzzle = new(puzzle.Id, puzzle.Name!, puzzle.ImageUrl!, puzzle.BasePrice);
+        // Try to the given puzzle in the cart
         CartPuzzle existingPuzzle = Cart.CartPuzzles.Find(p => p.Id == puzzle.Id)!;
+        // If the cart already has the given puzzle
         if (Cart.CartPuzzles.Find(p => p.Id == puzzle.Id) != null)
-        { existingPuzzle!.Quantity++; }
+        {
+            // Increase the quantity of the puzzle in the cart 
+            existingPuzzle!.Quantity++;
+        }
         else
-        { Cart.CartPuzzles.Add(newPuzzle); }
+        {
+            // Add the puzzle to the cart
+            Cart.CartPuzzles.Add(newPuzzle);
+        }
+        // Increase the cart total with the given cart's base price
         Cart.CartTotal += puzzle.BasePrice;
     }
 
+    /// <summary>
+    /// Removes the puzzle with the given Id, if it exists in the cart.
+    /// </summary>
+    /// <param name="puzzleId">The puzzle's Id.</param>
     public void RemoveFromCart(int puzzleId)
     {
+        // Find the puzzle with the given puzzleId
         CartPuzzle existingPuzzle = Cart.CartPuzzles.Find(p => p.Id == puzzleId)!;
+        // If the puzzle exisits
         if (existingPuzzle != null)
+            // Remove it from the cart
             Cart.CartPuzzles?.Remove(existingPuzzle);
     }
 }
